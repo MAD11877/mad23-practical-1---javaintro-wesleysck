3d1344c5751b480bc5c977fcd68e1ae16441f5c8
wesleysck <117621190+wesleysck@users.noreply.github.com>
2023-04-23 03:17:16 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -6,16 +6,19 @@ public class Question2
   
   public static void main(String[] args)
   {
-    import java.util.Scanner;
-    Scanner height = new Scanner(System.in);
-    System.out.print("");  
-    double h = height.nextDouble();
     
-    Scanner weight = new Scanner(System.in);
-    System.out.print("");  
-    double w = weight.nextDouble();
+    Scanner scanner = new Scanner(System.in);
+    
+    double weight;
+    double height;
 
-    double bmi = w / (h * h);
+    System.out.println("");
+    weight = scanner.nextDouble();
+
+    System.out.println("");
+    height = scanner.nextDouble();
+
+    double bmi = (weight / (height * height));
     
     System.out.print(bmi);  
   }
